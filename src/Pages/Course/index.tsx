2494615c8f0db96cde25eabb585d39527fe961e9
Connor Fech <cfech44@gmail.com>
2023-12-24 21:16:30 -0500

    small error handling added to course file [cf]

--- src/Pages/Course/index.tsx
+++ src/Pages/Course/index.tsx
@@ -1,17 +1,11 @@
 import { useEffect, useState } from "react";
-import { Box, Stack } from "@mui/material";
-import { fetchCourses, firebaseReadRealTime } from "../../utils/firebase-db.ts";
+import { Box, Stack, Typography } from "@mui/material";
+import { fetchCourses } from "../../utils/firebase-db.ts";
 import { CourseSummary } from "../../components/CourseSummary";
 import { ReviewCard } from "../../components/ReviewCard";
 
 export function Course() {
   const [courses, setCourses] = useState([]);
-
-  useEffect(() => {
-    console.log("Course");
-    // TODO - for some reason this is being called a lot, maybe just want to use getOnce
-    firebaseReadRealTime();
-  }, []);
 
   useEffect(() => {
     fetchCourses().then((courses) => {
@@ -23,9 +17,11 @@ export function Course() {
     <Stack alignItems="center">
       <CourseSummary />
       <Box>
-        {courses.map((course, idx) => (
-          <ReviewCard course={course} key={idx} />
-        ))}
+        {courses ? (
+          courses.map((course, idx) => <ReviewCard course={course} key={idx} />)
+        ) : (
+          <Typography>No Courses Yet</Typography>
+        )}
       </Box>
     </Stack>
   );
