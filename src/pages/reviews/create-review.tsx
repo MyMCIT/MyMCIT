e024a59a2002a28fa464fb8534f4fc4490cf96db
Michael Lwin <lwinm@seas.upenn.edu>
2024-02-02 01:23:47 +0100

    dummy to setup Vercel

--- src/pages/reviews/create-review.tsx
+++ src/pages/reviews/create-review.tsx
@@ -1,284 +1,284 @@
-import { SyntheticEvent, useEffect, useState } from "react";
-import {
-  Alert,
-  Box,
-  Button,
-  CircularProgress,
-  Container,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Snackbar,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { getSemesters } from "@/lib/semesters";
-import { GetStaticProps } from "next";
-import { useRouter } from "next/router";
-import { Course } from "@/models/course";
-import { supabase } from "@/lib/supabase";
-import Head from "next/head";
-
-export const getStaticProps: GetStaticProps = async () => {
-  let apiUrl;
-
-  if (process.env.NODE_ENV === "production") {
-    apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  } else {
-    apiUrl = "http://localhost:3000";
-  }
-  const res = await fetch(`${apiUrl}/api/courses`);
-  const courses = await res.json();
-
-  // sort the courses in alphabetical order
-  const sortedCourses: Course[] = courses.sort(
-    (a: { course_code: string }, b: { course_code: string }) =>
-      a.course_code.localeCompare(b.course_code),
-  );
-
-  return {
-    props: {
-      courses: sortedCourses,
-    },
-    revalidate: 86400,
-  };
-};
-
-export default function CreateReview({ courses }: any) {
-  const router = useRouter();
-  const [courseName, setCourseName] = useState<string>("");
-  const [course, setCourse] = useState<Course>();
-  const [semester, setSemester] = useState("");
-  const [difficulty, setDifficulty] = useState("");
-  const [workload, setWorkload] = useState<string>("");
-  const [rating, setRating] = useState("");
-  const [review, setReview] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [openAuthSnackbar, setOpenAuthSnackbar] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      const { data: session } = await supabase.auth.getSession();
-      if (!session.session?.user) {
-        setOpenAuthSnackbar(true);
-        router.push("/");
-      }
-    })();
-  }, [router]);
-
-  // submits review
-  const handleSubmit = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    const { data: sessionData } = await supabase.auth.getSession();
-
-    // check if there's an active session
-    if (!sessionData?.session) {
-      router.push("/"); // redirect to "/" if no active session
-      setIsSubmitting(false);
-      return;
-    }
-
-    const response = await fetch("/api/create-review", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${sessionData.session?.access_token}`,
-      },
-      body: JSON.stringify({
-        course_id: course?.id,
-        course_code: course?.course_code,
-        semester: semester,
-        difficulty: difficulty,
-        workload: workload + " hrs/wk",
-        rating: rating,
-        comment: review,
-      }),
-    });
-
-    setIsSubmitting(false);
-    const data = await response.json();
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    setOpenSnackbar(true);
-    await router.push("/");
-  };
-
-  const handleCloseSnackbar = (
-    event?: SyntheticEvent<Element, Event> | Event,
-    reason?: string,
-  ) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpenSnackbar(false);
-  };
-
-  const difficultyLevels = ["Very Easy", "Easy", "Medium", "Hard", "Very Hard"];
-  const ratings = [
-    "Strongly Disliked",
-    "Disliked",
-    "Neutral",
-    "Liked",
-    "Strongly Liked",
-  ];
-  const semesters = getSemesters();
-
-  return (
-    <>
-      <div>
-        <Head>
-          <title>Create Review</title>
-        </Head>
-      </div>
-      <Container maxWidth="sm">
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            mt: 2,
-          }}
-        >
-          <Typography variant="h6">Create Review</Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <FormControl fullWidth sx={{ my: 2 }}>
-              <InputLabel id="course-label">Course</InputLabel>
-              <Select
-                labelId="course-label"
-                value={courseName}
-                onChange={(e) => {
-                  // Find the selected course object and set it to your state
-                  const selectedCourse: Course = courses.find(
-                    (c: { course_code: string | undefined }) =>
-                      c.course_code === e.target.value,
-                  );
-                  setCourse(selectedCourse);
-                  setCourseName(selectedCourse.course_code);
-                }}
-                label="Course"
-              >
-                {courses.map((course: any) => (
-                  <MenuItem key={course.id} value={course.course_code}>
-                    {course.course_code}: {course.course_name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl fullWidth sx={{ my: 2 }}>
-              <InputLabel id="semester-label">Semester</InputLabel>
-              <Select
-                labelId="semester-label"
-                value={semester}
-                onChange={(e) => setSemester(e.target.value)}
-                label="Semester"
-              >
-                {semesters.map((season, i) => (
-                  <MenuItem key={i} value={season}>
-                    {season}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl fullWidth sx={{ my: 2 }}>
-              <InputLabel id="difficulty-label">Difficulty</InputLabel>
-              <Select
-                labelId="difficulty-label"
-                value={difficulty}
-                onChange={(e) => setDifficulty(e.target.value)}
-                label="Difficulty"
-              >
-                {difficultyLevels.map((level, i) => (
-                  <MenuItem key={i} value={level}>
-                    {level}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl fullWidth sx={{ my: 2 }}>
-              <TextField
-                type="number"
-                value={workload.toString()}
-                onChange={(e) => setWorkload(e.target.value)}
-                label="Workload (hours/week)"
-              />
-            </FormControl>
-            <FormControl fullWidth sx={{ my: 2 }}>
-              <InputLabel id="rating-label">Rating</InputLabel>
-              <Select
-                labelId="rating-label"
-                value={rating}
-                onChange={(e) => setRating(e.target.value)}
-                label="Rating"
-              >
-                {ratings.map((rating, i) => (
-                  <MenuItem key={i} value={rating}>
-                    {rating}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl fullWidth sx={{ my: 2 }}>
-              <TextField
-                multiline
-                rows={4}
-                value={review}
-                onChange={(e) => setReview(e.target.value)}
-                label="Your Review"
-              />
-            </FormControl>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              disabled={isSubmitting}
-            >
-              {isSubmitting ? <CircularProgress size={24} /> : "Create"}
-            </Button>
-          </Box>
-        </Box>
-        <Snackbar
-          open={openSnackbar}
-          autoHideDuration={4000}
-          onClose={handleCloseSnackbar}
-        >
-          <Alert
-            elevation={6}
-            variant="filled"
-            severity="success"
-            onClose={handleCloseSnackbar}
-          >
-            Successfully submitted new review!
-          </Alert>
-        </Snackbar>
-        <Snackbar
-          open={openAuthSnackbar}
-          autoHideDuration={9000}
-          onClose={handleCloseSnackbar}
-        >
-          <Alert
-            elevation={6}
-            variant="filled"
-            severity="error"
-            onClose={handleCloseSnackbar}
-          >
-            You are not authenticated!
-          </Alert>
-        </Snackbar>
-      </Container>
-    </>
-  );
-}
+// import { SyntheticEvent, useEffect, useState } from "react";
+// import {
+//   Alert,
+//   Box,
+//   Button,
+//   CircularProgress,
+//   Container,
+//   FormControl,
+//   InputLabel,
+//   MenuItem,
+//   Select,
+//   Snackbar,
+//   TextField,
+//   Typography,
+// } from "@mui/material";
+// import { getSemesters } from "@/lib/semesters";
+// import { GetStaticProps } from "next";
+// import { useRouter } from "next/router";
+// import { Course } from "@/models/course";
+// import { supabase } from "@/lib/supabase";
+// import Head from "next/head";
+//
+// export const getStaticProps: GetStaticProps = async () => {
+//   let apiUrl;
+//
+//   if (process.env.NODE_ENV === "production") {
+//     apiUrl = process.env.NEXT_PUBLIC_API_URL;
+//   } else {
+//     apiUrl = "http://localhost:3000";
+//   }
+//   const res = await fetch(`${apiUrl}/api/courses`);
+//   const courses = await res.json();
+//
+//   // sort the courses in alphabetical order
+//   const sortedCourses: Course[] = courses.sort(
+//     (a: { course_code: string }, b: { course_code: string }) =>
+//       a.course_code.localeCompare(b.course_code),
+//   );
+//
+//   return {
+//     props: {
+//       courses: sortedCourses,
+//     },
+//     revalidate: 86400,
+//   };
+// };
+//
+// export default function CreateReview({ courses }: any) {
+//   const router = useRouter();
+//   const [courseName, setCourseName] = useState<string>("");
+//   const [course, setCourse] = useState<Course>();
+//   const [semester, setSemester] = useState("");
+//   const [difficulty, setDifficulty] = useState("");
+//   const [workload, setWorkload] = useState<string>("");
+//   const [rating, setRating] = useState("");
+//   const [review, setReview] = useState("");
+//   const [isSubmitting, setIsSubmitting] = useState(false);
+//   const [openSnackbar, setOpenSnackbar] = useState(false);
+//   const [openAuthSnackbar, setOpenAuthSnackbar] = useState(false);
+//
+//   useEffect(() => {
+//     (async () => {
+//       const { data: session } = await supabase.auth.getSession();
+//       if (!session.session?.user) {
+//         setOpenAuthSnackbar(true);
+//         router.push("/");
+//       }
+//     })();
+//   }, [router]);
+//
+//   // submits review
+//   const handleSubmit = async (e: { preventDefault: () => void }) => {
+//     e.preventDefault();
+//     setIsSubmitting(true);
+//
+//     const { data: sessionData } = await supabase.auth.getSession();
+//
+//     // check if there's an active session
+//     if (!sessionData?.session) {
+//       router.push("/"); // redirect to "/" if no active session
+//       setIsSubmitting(false);
+//       return;
+//     }
+//
+//     const response = await fetch("/api/create-review", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${sessionData.session?.access_token}`,
+//       },
+//       body: JSON.stringify({
+//         course_id: course?.id,
+//         course_code: course?.course_code,
+//         semester: semester,
+//         difficulty: difficulty,
+//         workload: workload + " hrs/wk",
+//         rating: rating,
+//         comment: review,
+//       }),
+//     });
+//
+//     setIsSubmitting(false);
+//     const data = await response.json();
+//
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//
+//     setOpenSnackbar(true);
+//     await router.push("/");
+//   };
+//
+//   const handleCloseSnackbar = (
+//     event?: SyntheticEvent<Element, Event> | Event,
+//     reason?: string,
+//   ) => {
+//     if (reason === "clickaway") {
+//       return;
+//     }
+//
+//     setOpenSnackbar(false);
+//   };
+//
+//   const difficultyLevels = ["Very Easy", "Easy", "Medium", "Hard", "Very Hard"];
+//   const ratings = [
+//     "Strongly Disliked",
+//     "Disliked",
+//     "Neutral",
+//     "Liked",
+//     "Strongly Liked",
+//   ];
+//   const semesters = getSemesters();
+//
+//   return (
+//     <>
+//       <div>
+//         <Head>
+//           <title>Create Review</title>
+//         </Head>
+//       </div>
+//       <Container maxWidth="sm">
+//         <Box
+//           sx={{
+//             display: "flex",
+//             flexDirection: "column",
+//             alignItems: "center",
+//             mt: 2,
+//           }}
+//         >
+//           <Typography variant="h6">Create Review</Typography>
+//           <Box
+//             component="form"
+//             onSubmit={handleSubmit}
+//             noValidate
+//             sx={{ mt: 1 }}
+//           >
+//             <FormControl fullWidth sx={{ my: 2 }}>
+//               <InputLabel id="course-label">Course</InputLabel>
+//               <Select
+//                 labelId="course-label"
+//                 value={courseName}
+//                 onChange={(e) => {
+//                   // Find the selected course object and set it to your state
+//                   const selectedCourse: Course = courses.find(
+//                     (c: { course_code: string | undefined }) =>
+//                       c.course_code === e.target.value,
+//                   );
+//                   setCourse(selectedCourse);
+//                   setCourseName(selectedCourse.course_code);
+//                 }}
+//                 label="Course"
+//               >
+//                 {courses.map((course: any) => (
+//                   <MenuItem key={course.id} value={course.course_code}>
+//                     {course.course_code}: {course.course_name}
+//                   </MenuItem>
+//                 ))}
+//               </Select>
+//             </FormControl>
+//             <FormControl fullWidth sx={{ my: 2 }}>
+//               <InputLabel id="semester-label">Semester</InputLabel>
+//               <Select
+//                 labelId="semester-label"
+//                 value={semester}
+//                 onChange={(e) => setSemester(e.target.value)}
+//                 label="Semester"
+//               >
+//                 {semesters.map((season, i) => (
+//                   <MenuItem key={i} value={season}>
+//                     {season}
+//                   </MenuItem>
+//                 ))}
+//               </Select>
+//             </FormControl>
+//             <FormControl fullWidth sx={{ my: 2 }}>
+//               <InputLabel id="difficulty-label">Difficulty</InputLabel>
+//               <Select
+//                 labelId="difficulty-label"
+//                 value={difficulty}
+//                 onChange={(e) => setDifficulty(e.target.value)}
+//                 label="Difficulty"
+//               >
+//                 {difficultyLevels.map((level, i) => (
+//                   <MenuItem key={i} value={level}>
+//                     {level}
+//                   </MenuItem>
+//                 ))}
+//               </Select>
+//             </FormControl>
+//             <FormControl fullWidth sx={{ my: 2 }}>
+//               <TextField
+//                 type="number"
+//                 value={workload.toString()}
+//                 onChange={(e) => setWorkload(e.target.value)}
+//                 label="Workload (hours/week)"
+//               />
+//             </FormControl>
+//             <FormControl fullWidth sx={{ my: 2 }}>
+//               <InputLabel id="rating-label">Rating</InputLabel>
+//               <Select
+//                 labelId="rating-label"
+//                 value={rating}
+//                 onChange={(e) => setRating(e.target.value)}
+//                 label="Rating"
+//               >
+//                 {ratings.map((rating, i) => (
+//                   <MenuItem key={i} value={rating}>
+//                     {rating}
+//                   </MenuItem>
+//                 ))}
+//               </Select>
+//             </FormControl>
+//             <FormControl fullWidth sx={{ my: 2 }}>
+//               <TextField
+//                 multiline
+//                 rows={4}
+//                 value={review}
+//                 onChange={(e) => setReview(e.target.value)}
+//                 label="Your Review"
+//               />
+//             </FormControl>
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               sx={{ mt: 3, mb: 2 }}
+//               disabled={isSubmitting}
+//             >
+//               {isSubmitting ? <CircularProgress size={24} /> : "Create"}
+//             </Button>
+//           </Box>
+//         </Box>
+//         <Snackbar
+//           open={openSnackbar}
+//           autoHideDuration={4000}
+//           onClose={handleCloseSnackbar}
+//         >
+//           <Alert
+//             elevation={6}
+//             variant="filled"
+//             severity="success"
+//             onClose={handleCloseSnackbar}
+//           >
+//             Successfully submitted new review!
+//           </Alert>
+//         </Snackbar>
+//         <Snackbar
+//           open={openAuthSnackbar}
+//           autoHideDuration={9000}
+//           onClose={handleCloseSnackbar}
+//         >
+//           <Alert
+//             elevation={6}
+//             variant="filled"
+//             severity="error"
+//             onClose={handleCloseSnackbar}
+//           >
+//             You are not authenticated!
+//           </Alert>
+//         </Snackbar>
+//       </Container>
+//     </>
+//   );
+// }
