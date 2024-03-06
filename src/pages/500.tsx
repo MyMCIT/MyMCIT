7eeca0af5dc42826426ec026907afbbb525edda8
Kris Stern <krisstern@outlook.com>
2024-03-06 19:35:17 +0800

    fix 500.tsx

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -2,6 +2,6 @@
 
 import {Typography} from "@mui/material";
 
-export default function Custom404() {
+export default function Custom500() {
   return <><Typography variant="h1">500 - Server-side error occurred</Typography></>
 }
